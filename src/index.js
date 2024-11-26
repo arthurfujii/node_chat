e319fb45a4b37db25806db9681ec965c3353d423
Arthur Fujii <arthurfujii@gmail.com>
2024-11-26 13:51:05 -0300

    join room implemented

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@
 require('dotenv/config');
 
 const express = require('express');
+const { Server } = require('socket.io');
 const cors = require('cors');
-const EventEmitter = require('events');
-const { WebSocketServer } = require('ws');
 
 const PORT = process.env.PORT;
-let messages = [];
+const ADMIN = 'Admin';
+
+const messages = [];
 let users = [];
 let rooms = [];
 
@@ -17,176 +18,98 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const emitter = new EventEmitter();
-
-app.post('/users', (req, res) => {
-  const { username } = req.body;
-
-  if (!users.some((u) => u.username === username)) {
-    const user = {
-      id: users.length ? users[users.length - 1].id + 1 : 1,
-      username,
-      room: null,
-    };
-
-    users.push(user);
-    res.status(201).send(user);
-  } else {
-    res.status(400).send('Username already exists');
-  }
-});
-
-app.get('/allusers', (req, res) => {
+app.get('/users', (req, res) => {
   res.status(200).send(users);
 });
 
-app.get('/users', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Connection', 'keep-alive');
-  res.setHeader('Cache-Control', 'no-store');
-
-  const callback = () => {
-    res.write(`data: ${JSON.stringify(users)}\n\n`);
-  };
-
-  emitter.on('users', callback);
-
-  res.on('close', () => {
-    emitter.off('users', callback);
-  });
-});
-
-app.patch('/users/:id', (req, res) => {
-  const { id } = req.params;
-  const { roomId } = req.body;
-
-  const user = users.find((usr) => usr.id === +id);
-
-  if (!user) {
-    res.status(404).send('User not found');
-
-    return;
-  }
-
-  user.room = roomId;
-  res.status(201).send(user);
-});
-
-app.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
-
-  if (!users.some((usr) => +usr.id === +id)) {
-    res.status(404).send('User not found');
-
-    return;
-  }
-
-  const newUsers = users.filter((usr) => usr.id !== +id);
-
-  users = newUsers;
-  res.sendStatus(204);
-});
-
-app.post('/rooms', (req, res) => {
-  const { roomName } = req.body;
-
-  if (rooms.some((r) => r.roomName === roomName)) {
-    res.status(400).send('Room already exists');
-
-    return;
-  }
-
-  const room = {
-    id: rooms.length ? rooms[rooms.length - 1].id + 1 : 1,
-    roomName,
-    users: [],
-  };
-
-  rooms.push(room);
-  emitter.emit('rooms', rooms);
-  // eslint-disable-next-line no-console
-  console.log(rooms); // TODO: remove console.log
-  res.status(201).send(room);
-});
-
-app.get('/allrooms', (req, res) => {
+app.get('/rooms', (req, res) => {
   res.status(200).send(rooms);
 });
 
-app.get('/rooms', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Connection', 'keep-alive');
-  res.setHeader('Cache-Control', 'no-store');
+const server = app.listen(PORT);
+const io = new Server(server, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? false
+        : ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  },
+});
 
-  const callback = () => {
-    res.write(`data: ${JSON.stringify(rooms)}\n\n`);
-  };
+io.on('connection', (client) => {
+  // eslint-disable-next-line no-console
+  console.log(`User ${client.id} connected`); // TODO remove console.log
 
-  emitter.on('rooms', callback);
+  client.on('disconnect', () => {
+    // eslint-disable-next-line no-console
+    console.log(`User ${client.id} disconnected`); // TODO remove console.log
+  });
 
-  res.on('close', () => {
-    emitter.off('rooms', callback);
+  client.on('user', (data) => {
+    // if (users.find((usr) => usr.username === data)) {
+    //   client.emit('userError', 'Username already exists');
+
+    //   return;
+    // }
+
+    const user = {
+      id: client.id,
+      username: data,
+      currentRoom: null,
+    };
+
+    users.push(user);
+    client.emit('user', user);
+  });
+
+  client.on('deleteUser', (data) => {
+    users = users.filter((usr) => usr.id !== data);
+    io.emit('users', users);
+  });
+
+  client.on('joinRoom', async (data) => {
+    updateUser(data.user.id, data.room);
+    client.join(data.room.name);
+    io.emit('users', users);
+  });
+
+  client.on('room', async (data) => {
+    const room = {
+      id: generateId(rooms),
+      name: data,
+    };
+
+    rooms = [...rooms, room];
+    io.emit('rooms', rooms);
   });
 });
 
-app.delete('/rooms/:id', (req, res) => {
-  const { id } = req.params;
-
-  if (!rooms.some((r) => +r.id === +id)) {
-    res.status(404).send('Room not found');
-
-    return;
-  }
-
-  const newRooms = rooms.filter((r) => r.id !== +id);
-
-  rooms = newRooms;
-  res.sendStatus(204);
-});
-
-app.patch('/rooms/:id', (req, res) => {
-  const { id } = req.params;
-  const { updatedUser } = req.body;
-  const room = rooms.find((r) => r.id === +id);
-
-  if (!room) {
-    res.status(404).send('Room not found');
-
-    return;
-  }
-
-  room.users.push(updatedUser.data);
-
-  // eslint-disable-next-line no-console
-  console.log(updatedUser.data); // TODO: remove console.log
-  res.status(201).send(room);
-});
-
-app.get('/messages', (req, res) => {
-  res.status(200).send(messages);
-});
-
-const server = app.listen(PORT);
-const wss = new WebSocketServer({ server });
-
-wss.on('connection', (client, req) => {
-  client.on('message', (data) => {
-    const message = JSON.parse(data);
-
-    message.time = new Intl.DateTimeFormat('pt-BR', {
+function buildMsg(text, userId) {
+  return {
+    text,
+    userId,
+    time: new Intl.DateTimeFormat('pt-BR', {
       hour: 'numeric',
       minute: 'numeric',
       second: 'numeric',
-    }).format(new Date());
+    }).format(new Date()),
+  };
+}
 
-    messages = [message, ...messages];
+function updateUser(id, room) {
+  try {
+    const user = users.find((usr) => usr.id === id);
 
-    emitter.emit('message', JSON.stringify(messages));
-  });
-});
-
-emitter.on('message', (data) => {
-  for (const client of wss.clients) {
-    client.send(data);
+    user.currentRoom = room;
+  } catch (e) {
+    console.error(e);
   }
-});
+}
+
+function generateId(array) {
+  if (array.length) {
+    return array[array.length - 1].id + 1;
+  }
+
+  return 1;
+}
